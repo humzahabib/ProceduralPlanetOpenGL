ba62eff6d7104611e9c1eff5d2337cc24a056005
humzahabib <muhammadhamzahabib@outlook.com>
2026-07-23 00:30:04 +0500

    Made a corona sun, and experimented with some HDR stuff. I should get started on atmospheric scattering instead of getting distracted.

---
+++ include/shader.h
@@ -0,0 +1,35 @@
+//
+// Created by hhabib on 26/06/2026.
+//
+
+#ifndef SHADER_H
+#define SHADER_H
+
+#include "./glad.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Shader {
+public:
+    // the program ID
+    unsigned int ID;
+
+    // constructor reads and builds the shader
+    Shader(const char* vertexPath, const char* fragmentPath);
+    // use/activate the shader
+    void use() const;
+    // utility uniform functions
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
+    void setFloat(const std::string &name, float value) const;
+    void setVec3(const std::string &name, glm::vec3 value);
+    void setMat4(const std::string &name, glm::mat4 &value);
+};
+
+#endif
+
+
